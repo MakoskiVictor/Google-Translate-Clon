a544d8b5186d4e6e746f9e75d219683e0dbe1e1f
MakoskiVictor <makoski.ed@gmail.com>
2023-04-04 17:04:38 -0300

    added Select and andleChange

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,7 +1,22 @@
 import { Form } from 'react-bootstrap'
-import { SUPPORTED_LANGUAGES } from '@/constants'
-export function LanguageSelector({ onChange }) {
+import { AUTO_LANGUAGE, SUPPORTED_LANGUAGES } from '@/constants'
+import { SectionType, type Language, type Props } from '@/types.d'
+
+export function LanguageSelector({ onChange, type, value }: Props) {
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    onChange(e.target.value as Language)
+  }
+
   return (
-        <div>asd</div>
+        <Form.Select aria-label='Select language' onChange={handleChange} value={value}>
+            {type === SectionType.From && <option value={AUTO_LANGUAGE}>Auto</option>}
+
+            {Object.entries(SUPPORTED_LANGUAGES).map(([key, literal]) => (
+                <option key={key} value={key}>
+                    {literal}
+                </option>
+            ))
+            }
+        </Form.Select>
   )
 }
