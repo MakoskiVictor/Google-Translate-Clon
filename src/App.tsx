4824030b1398d889780106902fbdf90693250599
MakoskiVictor <makoski.ed@gmail.com>
2023-04-04 16:08:55 -0300

    added  containers and colums

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Col, Row, Button } from 'react-bootstrap'
 import { useStore } from '@/hooks/useStore'
+import { AUTO_LANGUAGE } from './constants'
+import { ArrowIcon } from '@/components/Icons'
 
 function App() {
-  const { fromLanguage, setFromLanguage } = useStore()
+  const { fromLanguage, toLanguage, interchangeLanguages } = useStore()
   return (
-    <div className="App">
+    <Container fluid>
       <h1>Google Translate</h1>
-    </div>
+
+      <Row>
+        <Col>
+          <h2>From</h2>
+          {fromLanguage}
+        </Col>
+
+        <Col>
+          <Button variant='link' disabled={fromLanguage === AUTO_LANGUAGE} onClick={interchangeLanguages}>
+            <ArrowIcon />
+          </Button>
+        </Col>
+
+        <Col>
+          <h2>To</h2>
+          {toLanguage}
+        </Col>
+      </Row>
+
+    </Container>
   )
 }
 
