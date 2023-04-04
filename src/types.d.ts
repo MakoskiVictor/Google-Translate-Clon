d807ee6cec5e2bef36a011b5a85e94cbf04967f8
MakoskiVictor <makoski.ed@gmail.com>
2023-04-04 12:41:44 -0300

    created types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,5 @@
+import { type AUTO_LANGUAGE, type SUPPORTED_LANGUAGES } from '@/constants'
+
 export interface State {
   fromLanguage: string
   toLanguage: string
@@ -5,3 +7,17 @@ export interface State {
   result: string
   loading: boolean
 }
+
+export type Action =
+  | { type: 'INTERCHANGE_LANGUAGES' }
+  | { type: 'SET_FROM_LANGUAGE', payload: string }
+  | { type: 'SET_TO_LANGUAGE', payload: string }
+  | { type: 'SET_FROM_TEXT', payload: string }
+  | { type: 'SET_RESULT', payload: string }
+
+// keyof typeof => el tipo es una de las keys del objeto supported_languages
+export type Language = keyof typeof SUPPORTED_LANGUAGES
+
+export type AutoLanguage = typeof AUTO_LANGUAGE
+
+export type FromLanguage = Language | AutoLanguage
