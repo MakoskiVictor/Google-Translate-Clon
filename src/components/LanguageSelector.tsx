3950563d14157f6e24ffcc4968fa21b1b46361d2
MakoskiVictor <makoski.ed@gmail.com>
2023-04-04 16:07:35 -0300

    created language selector

---
+++ src/components/LanguageSelector.tsx
@@ -0,0 +1,7 @@
+import { Form } from 'react-bootstrap'
+import { SUPPORTED_LANGUAGES } from '@/constants'
+export function LanguageSelector({ onChange }) {
+  return (
+        <div>asd</div>
+  )
+}
