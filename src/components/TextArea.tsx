04dd653ad642c9d3a70f6fdff6a97a470acbe087
MakoskiVictor <makoski.ed@gmail.com>
2023-04-05 16:11:49 -0300

    created teaxtArea component

---
+++ src/components/TextArea.tsx
@@ -0,0 +1,38 @@
+import { SectionType } from '@/types.d'
+import { Form } from 'react-bootstrap'
+
+interface Props {
+  type: SectionType
+  loading?: boolean
+  onChange: (value: string) => void
+  value: string
+}
+
+const commonStyles = { border: 0, height: '200px', resize: 'none' } as const
+
+const getPlaceholder = ({ type, loading }: { type: SectionType, loading?: boolean }) => {
+  if (type === SectionType.From) return 'Enter text'
+  if (loading === true) return 'Translating'
+  return 'Translation'
+}
+
+export function TextArea ({ type, loading, value, onChange }: Props) {
+  const styles = type === SectionType.From
+    ? commonStyles
+    : { ...commonStyles, backgroundColor: '#f5f5f5' }
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    onChange(e.target.value)
+  }
+
+  return (
+        <Form.Control
+            as='textarea'
+            placeholder={getPlaceholder({ type, loading })}
+            autoFocus={type === SectionType.From}
+            style={styles}
+            value={value}
+            onChange={handleChange}
+            />
+  )
+}
