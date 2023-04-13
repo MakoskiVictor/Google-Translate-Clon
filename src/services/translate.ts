4d517a9d8afd61ee40a4c92a2cc87467eaf507ea
MakoskiVictor <makoski.ed@gmail.com>
2023-04-13 16:19:01 -0300

    change method

--- src/services/translate.ts
+++ src/services/translate.ts
@@ -1,17 +1,20 @@
 import { type FromLanguage, type Language } from '@/types'
+/* import { API_URL } from '@/constants' */
 
 export async function translate ({ fromLanguage, toLanguage, text }:
 { fromLanguage: FromLanguage, toLanguage: Language, text: string }): Promise<string> {
   try {
-    const translation = await fetch('localhost3000/translate', {
-      method: 'GET',
+    const translation = await fetch('http://localhost:3000/translate', {
+      method: 'POST',
       body: JSON.stringify({ fromLanguage, toLanguage, text }),
       headers: {
         'Content-Type': 'application/json'
       }
     })
-      .then(async res => await res.json())
-    return translation
+
+    const result = translation.json()
+
+    return await result
   } catch (error) {
     console.error('Error:', error)
     return 'Límite de pedidos alcanzado / Order limit reached'
