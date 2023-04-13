637fc8c8fc77e467e7e83ae9fbb884068dba0884
MakoskiVictor <makoski.ed@gmail.com>
2023-04-13 16:20:41 -0300

    Added SPEECH_LANGUAGES

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,11 @@ export const SUPPORTED_LANGUAGES = {
 }
 
 export const AUTO_LANGUAGE = 'auto'
+
+export const SPEECH_LANGUAGES = {
+  en: 'en-GB',
+  es: 'es-AR',
+  de: 'de-DE'
+}
+
+export const API_URL = 'https://googletranslateapi.onrender.com/'
