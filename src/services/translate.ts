fdd38ae7e6af23daeca18e9406b46e77c6b68f31
MakoskiVictor <makoski.ed@gmail.com>
2023-04-16 23:45:45 -0300

    change credentials

--- src/services/translate.ts
+++ src/services/translate.ts
@@ -7,7 +7,7 @@ export async function translate ({ fromLanguage, toLanguage, text }:
     const translation = await fetch(`${API_URL}/translate`, {
       method: 'POST',
       mode: 'cors',
-      credentials: 'include',
+      credentials: 'same-origin',
       body: JSON.stringify({ fromLanguage, toLanguage, text }),
       headers: {
         'Content-Type': 'application/json'
