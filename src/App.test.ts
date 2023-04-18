f223c78964650a0a8192534b51dc63e39b6bb2a8
MakoskiVictor <makoski.ed@gmail.com>
2023-04-18 05:07:03 -0300

    test App

---
+++ src/App.test.ts
@@ -0,0 +1,16 @@
+import { expect, test } from 'vitest'
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import App  from '@/App'
+
+test('All works well', async () => {
+    const app = render(<App />)
+
+    const user = userEvent.setup()
+    const textareaFrom = app.getByDisplayValue('Enter text')
+
+    await user.type(textareaFrom, 'Hola mundo')
+    const result = await app.findByDisplayValue(/Hello world/i, {}, {timeout: 5000})
+
+    expect(result).toBeTruthy()
+})
